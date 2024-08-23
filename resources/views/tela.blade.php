5c9bfbdab3766dec0f7465ece6eec7cb62d480b1
ewbriao1978 <eduardo.briao@gmail.com>
2024-08-23 14:27:52 -0300

    Add flashdata messagens

--- resources/views/tela.blade.php
+++ resources/views/tela.blade.php
@@ -8,6 +8,16 @@
 <body>
     <h1> Listagem de produtos </h1>
 
+    @if (session()->has('success'))
+    <strong> <font color="Navy"> {{ session('success') }} <font> </strong>
+    <br>
+    @elseif (session()->has('success_removing'))
+    <strong> <font color="RoyalBlue"> {{ session('success_removing')}} <font> </strong>
+    @elseif (session()->has('success_update'))
+    <strong> <font color="SteelBlue"> {{ session('success_update') }} <font> </strong>
+
+    @endif
+
     <br>
     <a href=" {{ url('/inserir') }}"> Inserir dados </a>
     <br>
